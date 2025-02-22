d9155181a4f04c589f09b916908b57709c567f99
Byr3n <sealunion45@gmail.com>
2025-02-21 18:11:07 -0800

    first commit

---
+++ Activity.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the number whose sum you want to find:"))
+sum=0
+for byran in range(1,n+1):
+    sum=sum+byran
+    print("\n Sum=",sum)
---
+++ Activity2.py
@@ -0,0 +1,6 @@
+string=input("Enter the string:")
+string2=''
+for i in string:
+    string2=i+string2
+print(string2)
+print("Reverse string:",string2)
